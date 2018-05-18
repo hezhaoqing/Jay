e25197df3058ee1abf523424c725760c46f69413
Papaya <307798536@qq.com>
2018-05-18 09:15:24 +0800

    Update example_1.py

--- python/learn/example_1.py
+++ python/learn/example_1.py
@@ -5,23 +5,23 @@ import os
 import platform
 import time
 import random
-from file import *                                        ###### 从某文件导入。比如在某个文件定义一些字典。
-from threading import Thread                              ###### 线程. 从模块导入类。
+from file import *                                  		       	  ###### 从某文件导入。比如在某个文件定义一些字典。
+from threading import Thread                    		  	  ###### 线程. 从模块导入类。
 
 DIR = "/opt/hezhaoqing"                
 
-def main():
+def main():								 ###### 定义一个没有参数的函数
 	if not os.path.exists(DIR)
 		os.mkdir(DIR)
 
-	print(platform.system())                              ###### Linux
+	print(platform.system())                        	         ###### Linux
 
-	print(time.strftime("%Y-%m-%d %H:%M:%S"))	          ###### 2018-05-20 20:13:14
+	print(time.strftime("%Y-%m-%d %H:%M:%S"))	                 ###### 2018-05-20 20:13:14
 
-	gameserverip = info['host']['gameserver'][userid]     ###### 取嵌套字典的某个value
-	
-	
+	print(info['host']['gameserver']['userid'])                      ###### 取嵌套字典的某个value : 字典名[key][key][key][key].
 	
 
-if __name__ == "__main__":                                ###### 被调用不执行，被直接执行才执行。
+
+
+if __name__ == "__main__":                                               ###### 被调用不执行，被直接执行才执行。
 	main()
