7f0fd0ec053eb311af38cc3daf18dbcbccd50ba0
Papaya <307798536@qq.com>
2018-05-18 09:38:02 +0800

    Update example_1.py

--- python/learn/example_1.py
+++ python/learn/example_1.py
@@ -7,6 +7,10 @@ import time
 import random
 from file import *                                  		       	  ###### 从某文件导入。比如在某个文件定义一些字典。
 from threading import Thread                    		  	  ###### 线程. 从模块导入类。
+import sys
+
+
+
 
 DIR = "/opt/hezhaoqing"                
 
@@ -17,11 +21,15 @@ def main():								 ###### 定义一个没有参数的函数
 	print(platform.system())                        	         ###### Linux
 
 	print(time.strftime("%Y-%m-%d %H:%M:%S"))	                 ###### 2018-05-20 20:13:14
-
+	time.sleep(1)							 ###### 休眠1秒
+	
 	print(info['host']['gameserver']['userid'])                      ###### 取嵌套字典的某个value : 字典名[key][key][key][key].
 	
-
+	print (myargv)
 
 
 if __name__ == "__main__":                                               ###### 被调用不执行，被直接执行才执行。
+
+	myargv= sys.argv[1]						 ###### 定义上面要打印的myargv 为脚本的第一个参数
+	
 	main()
