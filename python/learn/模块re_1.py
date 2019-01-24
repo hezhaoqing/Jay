bb5df8bc45658537156f13c4a582b83ef2d07325
Papaya <307798536@qq.com>
2019-01-24 10:26:09 +0800

    Update 模块re_1.py

--- python/learn/模块re_1.py
+++ python/learn/模块re_1.py
@@ -14,6 +14,10 @@ re.subn(pattern, repl, string[, count])
 
 
 （一）re.match(pattern, string[, flags])
+从string（我们要匹配的字符串）的开头开始，尝试匹配pattern，一直向后匹配，
+如果遇到无法匹配的字符，立即返回None，
+如果匹配未结束已经到达string的末尾，也会返回None。
+两个结果均表示匹配失败，否则匹配pattern成功，同时匹配终止，不再对string向后匹配
 # -*- coding: utf-8 -*- 
 #导入re模块
 import re 
@@ -58,3 +62,67 @@ search方法与match方法极其类似，
 search()会扫描整个string查找匹配，
 match（）只有在0位置匹配成功的话才有返回，如果不是开始位置匹配成功的话，match()就返回None。
 同样，search方法的返回对象同样有match()返回对象的方法和属性
+
+
+
+
+（三）re.split(pattern, string[, maxsplit])
+按照能够匹配的子串将string分割后返回列表。maxsplit用于指定最大分割次数，不指定将全部分割。
+import re 
+pattern = re.compile(r'\d+')
+print re.split(pattern,'one1two222three33four4five')
+
+
+
+
+（四）re.findall(pattern, string[, flags])
+以列表形式返回全部能匹配的子串
+import re 
+pattern = re.compile(r'\d+')
+print re.findall(pattern,'one1two2three3four4')
+
+
+
+
+（五）re.finditer(pattern, string[, flags])
+返回一个顺序访问每一个匹配结果（Match对象）的迭代器
+import re 
+pattern = re.compile(r'\d+')
+for m in re.finditer(pattern,'one1two2three3four4'):
+    print m.group(),
+
+
+    
+ 
+（六）re.sub(pattern, repl, string[, count])
+使用repl替换string中每一个匹配的子串后返回替换后的字符串。
+当repl是一个字符串时，可以使用\id或\g、\g引用分组，但不能使用编号0。
+当repl是一个方法时，这个方法应当只接受一个参数（Match对象），并返回一个字符串用于替换（返回的字符串中不能再引用分组）。
+count用于指定最多替换次数，不指定时全部替换。    
+import re
+pattern = re.compile(r'(\w+) (\w+)')
+s = 'i say, hello world!' 
+print re.sub(pattern,r'\2 \1', s)
+def func(m):
+    return m.group(1).title() + ' ' + m.group(2).title()
+print re.sub(pattern,func, s)
+### output ###
+# say i, world hello!
+# I Say, Hello World!    
+    
+    
+    
+    
+（七）re.subn(pattern, repl, string[, count])    
+ 返回 (sub(repl, string[, count]), 替换次数)。   
+    
+    
+
+
+
+
+*** Python Re模块的另一种使用方式 ***
+在上面7个方法的调用方式都是 re.match，re.search的方式，
+其实还有另外一种调用方式，可以通过pattern.match，pattern.search调用，
+这样调用便不用将pattern作为第一个参数传入了，想怎样调用皆可。
+***
